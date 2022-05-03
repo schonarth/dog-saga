99fc6b1d602f2b4cbb95cda09ecfd8f9fb874c3d
Gustavo Schonarth <gustavo@schonarth.com.br>
2022-05-03 20:03:46 +0000

    Implement Redux-Saga to fetch dog pictures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { API_CALL_REQUEST } from "./redux";
+import { connect } from "react-redux";
 
-function App() {
+function App({ fetching, dog, onRequestDog, error }) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={dog || logo} className="App-logo" alt="logo" />
+        <h1 className="app-title">Welcome to the Dog Saga</h1>
+
+        {dog ? (
+          <p className="app-intro">Keep clicking for new dogs</p>
+        ) : (
+          <p className="app-intro">Replace the React icon with a dog!</p>
+        )}
+
+        {fetching ? (
+          <button disabled>Fetching...</button>
+        ) : (
+          <button onClick={onRequestDog}>Request a Dog</button>
+        )}
+        {error && <p style={{ color: "red" }}>Uh oh - something went wrong!</p>}
       </header>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    fetching: state.fetching,
+    dog: state.dog,
+    error: state.error,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onRequestDog: () => dispatch({ type: API_CALL_REQUEST }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import registerServiceWorker from "./registerServiceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+import { reducer } from "./redux";
+import { watcherSaga } from "./sagas";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// dev tools middleware
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// create a redux store with our reducer above and middleware
+let store = createStore(
+  reducer,
+  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+);
+
+// run the saga
+sagaMiddleware.run(watcherSaga);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
@@ -15,3 +39,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// registerServiceWorker();
