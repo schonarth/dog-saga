01e54fc187ac50308101b6d96257dbf5c16c85b9
Gustavo Schonarth <gustavo@schonarth.com.br>
2022-05-08 16:30:40 +0000

    Successfully make Jest run
    With React. Jest dependencies versions and all. Now to add the tests proper

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import App from "./App";
+
+test("um mais um é dois", () => {
+  expect(1 + 1).toBe(2);
+});
 
 describe("main page layout", () => {
   test("renders the title and main paragraph", () => {
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     const titleElement = screen.getByText(/Welcome to the Dog Saga/i);
     expect(titleElement).toBeInTheDocument();
     const paraElement = screen.getByText(/Keep clicking/i);
---
+++ src/babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    "@babel/plugin-transform-react-jsx",
+    "@babel/proposal-class-properties",
+    "@babel/plugin-transform-runtime",
+  ],
+};
